e8614e7323103e39c8beae4b2dd662addd5da376
lajos.pajtek <lajos.pajtek@localhost>
2009-10-08 10:23:03 +0200

    cosmetics

--- python/projecteuler/p024.py
+++ python/projecteuler/p024.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python
 
+n=10
+digits = range(0,n)
+m = 1000000
+
 def f(n):
     if (n<=1):
         return 1
     else:
        return n*f(n-1)
 
-digits = [0,1,2,3,4,5,6,7,8,9]
-#digits = [0,1,2]
-ans = []
-
-s = map(f,range(len(digits)-1,-1,-1))
-print s
-
-m = 1000000
-#m = 4
 m -= 1
-
+ans = []
+s = map(f,range(len(digits)-1,-1,-1))
 i = iter(s)
-#while (m>0):
 while (len(digits)>0):
     ii = i.next()
     (n, m) = divmod(m,ii)
@@ -29,7 +24,6 @@ while (len(digits)>0):
 
 import string
 print string.join(map(str,ans),'')
-#print "correct: 2783915460"
 
 # cheat from:
 # http://blog.dreamshire.com/2009/05/22/project-euler-problem-24-solution/
