e37dc45f36fe6c4047369275ccc6e925da807c1d
lajos.pajtek <lajos.pajtek@localhost>
2009-10-08 10:35:12 +0200

    p024: added prpoblem description

--- python/projecteuler/p024.py
+++ python/projecteuler/p024.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+# A permutation is an ordered arrangement of objects. For example, 3124 is one 
+# possible permutation of the digits 1, 2, 3 and 4. If all of the permutations 
+# are listed numerically or alphabetically, we call it lexicographic order. 
+# The lexicographic permutations of 0, 1 and 2 are:
+#
+#     012   021   102   120   201   210
+#
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 
+# 5, 6, 7, 8 and 9?
+#
+# Answer: 2783915460
+
 n=10
 digits = range(0,n)
 m = 1000000
@@ -27,5 +39,4 @@ print string.join(map(str,ans),'')
 
 # cheat from:
 # http://blog.dreamshire.com/2009/05/22/project-euler-problem-24-solution/
-# 2783915460
 
